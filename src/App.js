10b8dc0860a1ab2f451f6ad0d9eec7925f373af8
ZloyLeva <zloyleva@gmail.com>
2019-02-10 19:19:23 +0200

    crerate addForm component

--- src/App.js
+++ src/App.js
@@ -1,67 +1,15 @@
 import React, { Component } from 'react';
 
 import Header from "./Layouts/Header";
+import AddTask from "./components/AddTask";
 import TaskList from "./components/TaskList";
-import {addNewTask} from "./store/actions";
 
-import {connect} from 'react-redux';
-import {bindActionCreators} from "redux";
+export default props => (
+    <div>
+        <Header/>
 
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
+        <AddTask/>
 
-
-const styles = theme => ({
-    root: {
-        flexGrow: 1,
-    },
-});
-
-class App extends Component {
-    render() {
-        console.log(this.props);
-        const {tasks, addNewTask, classes} = this.props;
-
-        return (
-            <div className={classes.root}>
-
-                <Header/>
-
-                <Grid container spacing={24}>
-                    <form action="" onSubmit={(event)=>{
-                        this.submitHandler(event, addNewTask)
-                    }}>
-                        <input type="text"/>
-                        <button>Add task</button>
-                    </form>
-
-                    <TaskList tasks={tasks}/>
-                </Grid>
-            </div>
-        );
-    }
-
-    submitHandler = (e,addNewTask) => {
-        e.preventDefault();
-        console.log("submitHandler",e.target[0].value);
-        addNewTask(e.target[0].value);
-        e.target.reset();
-    }
-}
-
-const putStateToProps = (state) => {
-    return {
-        ...state
-    }
-};
-
-const putActionsToComponent = (dispatch) => {
-    return {
-        addNewTask: bindActionCreators(addNewTask,dispatch)
-    }
-};
-
-export default withStyles(styles)(connect(
-    putStateToProps,
-    putActionsToComponent
-)(App));
+        <TaskList tasks={props.tasks}/>
+    </div>
+);
---
+++ src/components/AddTask/index.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+import connect from "react-redux/es/connect/connect";
+import {bindActionCreators} from "redux";
+import {addNewTask} from "../../store/actions";
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    container: {
+        justifyContent: "center"
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'center',
+    },
+    textField:{
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+    },
+    button: {
+        margin: theme.spacing.unit,
+    },
+    form: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    }
+});
+
+class AddTask extends Component{
+
+    state = {
+        task: ""
+    };
+
+    render() {
+        {
+            const {classes,addNewTask} = this.props;
+            return (
+                <div className={classes.root}>
+                    <Grid container spacing={24} className={classes.container}>
+                        <Grid item xs={10} sm={8} md={6}>
+                            <Paper className={classes.paper}>
+                                <form className={classes.form} onSubmit={(event) => {
+                                    this.submitHandler(event, addNewTask)
+                                }}>
+                                    {/*<input type="text"/>*/}
+                                    <TextField
+                                        id="outlined-name"
+                                        label="Enter new Task"
+                                        className={classes.textField}
+                                        value={this.state.task}
+                                        onChange={this.handleChange}
+                                        margin="normal"
+                                        variant="outlined"
+                                    />
+                                    {/*<button>Add task</button>*/}
+                                    <Button variant="outlined" color="primary" className={classes.button}>
+                                        Add task
+                                    </Button>
+                                </form>
+                            </Paper>
+
+                        </Grid>
+                    </Grid>
+                </div>
+            )
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            task: e.target.value
+        });
+    };
+
+    submitHandler = (e,addNewTask) => {
+        e.preventDefault();
+        console.log("submitHandler",e.target[0].value);
+        addNewTask(this.state.task);
+        this.setState({
+            task: ""
+        });
+    };
+}
+
+const putStateToProps = (state) => {
+    return {
+        ...state
+    }
+};
+
+const putActionsToComponent = (dispatch) => {
+    return {
+        addNewTask: bindActionCreators(addNewTask,dispatch)
+    }
+};
+
+export default withStyles(styles)(connect(
+    putStateToProps,
+    putActionsToComponent
+)(AddTask));
