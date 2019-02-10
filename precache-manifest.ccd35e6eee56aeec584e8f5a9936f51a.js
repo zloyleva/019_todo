bb90ae51177e9ed59439628895cdc9a67c8058ae
ZloyLeva <zloyleva@gmail.com>
2019-02-10 15:44:53 +0200

    Updates

---
+++ precache-manifest.ccd35e6eee56aeec584e8f5a9936f51a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/019_todo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a9e70ecc4ee94a3d0ea1",
+    "url": "/019_todo/static/js/runtime~main.a9e70ecc.js"
+  },
+  {
+    "revision": "d43bb3e534e9a0de06d2",
+    "url": "/019_todo/static/js/main.d43bb3e5.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/019_todo/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "d43bb3e534e9a0de06d2",
+    "url": "/019_todo/static/css/main.73a333fa.chunk.css"
+  },
+  {
+    "revision": "c577d83871c4f55636708a978d9b6834",
+    "url": "/019_todo/index.html"
+  }
+];
