51161d97d14631d400aea81964326abf83d3ab1f
ZloyLeva <zloyleva@gmail.com>
2019-02-17 16:55:08 +0200

    show tasks controls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Header from "./Layouts/Header";
 import AddTask from "./components/AddTask";
--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -1,25 +1,71 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 import {doneTask, removeTask} from "../../store/actions";
 import {connect} from "react-redux";
 
+import { withStyles } from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid/Grid";
+import Paper from "@material-ui/core/Paper/Paper";
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import DoneIcon from '@material-ui/icons/Done';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    container: {
+        justifyContent: "center"
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'center',
+    },
+    title: {
+        textAlign: "left"
+    },
+    taskDone:{
+        background: "#4caf50"
+    },
+    taskRemove:{
+        background: "#f44336"
+    }
+});
+
 const TaskList = (props) => {
 
-    const {tasks, setTaskDone, setTaskRemove} = props;
+    const {tasks, setTaskDone, setTaskRemove,classes} = props;
     console.log(props);
 
     return (
-        <ul>
+        <Fragment>
             {tasks.map((el, index) => (
-                <li key={index}>
-                    {el.name}
-                    {
-                        el.done?"":<button onClick={() => setTaskDone(index)}>Done</button>
-                    }
-                    <button onClick={() => setTaskRemove(index)}>Remove</button>
-                </li>
+                <div className={classes.root} key={index}>
+                    <Grid container spacing={24} className={classes.container}>
+                        <Grid item xs={10} sm={8} md={6}>
+                            <Paper className={classes.paper}>
+                                <Grid container spacing={24} className={classes.container}>
+                                    <Grid item xs={10}>
+                                        <Typography className={classes.title} component="h2" variant="h4" gutterBottom>
+                                            {el.name}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                        <IconButton className={classes.button} aria-label="Done" onClick={() => setTaskDone(index)}>
+                                            <DoneIcon />
+                                        </IconButton>
+                                        <IconButton className={classes.button} aria-label="Delete" onClick={() => setTaskRemove(index)}>
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    </Grid>
+                                </Grid>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </div>
             ))}
-        </ul>
+        </Fragment>
     )
 };
 
@@ -31,4 +77,4 @@ const mapActionsToProps = dispatch =>{
     }
 };
 
-export default connect(mapStateToProps,mapActionsToProps)(TaskList);
+export default withStyles(styles)(connect(mapStateToProps,mapActionsToProps)(TaskList));
